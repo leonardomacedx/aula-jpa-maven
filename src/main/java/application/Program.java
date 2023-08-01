46947de519c2c530d75964b753ed59460cfc6b7f
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-08-01 20:12:49 -0300

    Aulas finalizadas

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -8,22 +8,16 @@ import jakarta.persistence.Persistence;
 public class Program {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(null, "Leonardo Macedo", "leonardo@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Karol Mendes", "karol@gmail.com");
-        Pessoa p3 = new Pessoa(null, "José Silva", "jose@gmail.com");
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
-        EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+            Pessoa p = em.find(Pessoa.class, 2);
+            em.getTransaction().begin();
+            em.remove(p);
+            em.getTransaction().commit();
+
+        }
         System.out.println("Pronto!");
-
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
     }
 }
